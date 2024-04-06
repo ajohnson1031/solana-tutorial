d06c2a2eaa86a8fd5df314a6c905cd281df858cf
ajohnson1031 <aaron.johnson1031@gmail.com>
2024-04-06 17:45:55 -0400

    added ability to check any wallet address using devnet | mainnet flag

---
+++ check-any-balance.ts
@@ -0,0 +1,21 @@
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+
+const suppliedPublicKey = process.argv[2];
+
+if (!suppliedPublicKey) throw new Error("Please provide a public key.");
+
+try {
+  const env = process.argv[3] || "--D";
+  const connectionURLs = { "--D": "https://api.devnet.solana.com", "--M": "https://api.mainnet-beta.solana.com/" };
+
+  const connection = new Connection(connectionURLs[env], "confirmed");
+  const publicKey = new PublicKey(suppliedPublicKey);
+  const balanceInLamports = await connection.getBalance(publicKey);
+  const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+  console.log(`🔥 Finished! The balance for the wallet address at ${publicKey} is ${balanceInSOL}`);
+} catch (error) {
+  if (error.message === "Invalid public key input") {
+    console.error("🙁 Please provide a valid public key in the form of a base58 encoded string.");
+  } else console.error(error);
+}
